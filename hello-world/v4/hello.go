f3f8fb1e3f09bad4cd9c415ed765b822196e56a6
JohnnyB0Y <ag860050872@163.com>
2021-06-26 20:18:13 +0800

    hello-world v4 finished.

--- hello-world/v4/hello.go
+++ hello-world/v4/hello.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const chineseHelloPrefix = "泥猴，"
 
 // Hello returns a personalised greeting.
 func Hello(name string) string {
 	return englishHelloPrefix + name
+}
+
+func HelloGo(name string) string {
+	return chineseHelloPrefix + name
 }
 
 func main() {
--- hello-world/v4/hello_test.go
+++ hello-world/v4/hello_test.go
@@ -10,3 +10,14 @@ func TestHello(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestHelloGo(t *testing.T) {
+	got := HelloGo("Jack")
+	want := chineseHelloPrefix + "Jack"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	} else {
+		t.Log(want)
+	}
+}
