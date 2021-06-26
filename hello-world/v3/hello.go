1040f66a45f49c9a049573a877fba7f4318891f8
JohnnyB0Y <ag860050872@163.com>
2021-06-26 20:14:17 +0800

    hello-world v3 finished.

--- hello-world/v3/hello.go
+++ hello-world/v3/hello.go
@@ -7,6 +7,10 @@ func Hello(name string) string {
 	return "Hello, " + name
 }
 
+func HelloGo(name string) string {
+	return "Hello Go, " + name
+}
+
 func main() {
 	fmt.Println(Hello("world"))
 }
--- hello-world/v3/hello_test.go
+++ hello-world/v3/hello_test.go
@@ -10,3 +10,12 @@ func TestHello(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestHelloGo(t *testing.T) {
+	got := HelloGo("Chris")
+	want := "Hello Go, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
