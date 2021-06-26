270404bde5d32af21c6fb189753e53fbd0fcc123
JohnnyB0Y <ag860050872@163.com>
2021-06-26 20:50:10 +0800

    integers finished.

--- integers/v1/adder_test.go
+++ integers/v1/adder_test.go
@@ -10,3 +10,12 @@ func TestAdder(t *testing.T) {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
 }
+
+func TestAddFloat(t *testing.T) {
+	sum := AddFloat(3.4, 6)
+	var expected float32 = 9.4
+
+	if sum != expected {
+		t.Errorf("expected '%f' but got '%f'", expected, sum)
+	}
+}
